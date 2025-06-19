a0864feba116a4bc63f3e5ad7a9cb7d81c53cb21
Umar <umar@mac.home>
2025-06-19 21:57:38 +0100

    Added first round of testing

---
+++ test/instachain.js
@@ -0,0 +1,54 @@
+const { expect } = require('chai'); // Import expect from chai
+const { ethers } = require('hardhat'); // Import ethers from hardhat
+
+// Define the test suite
+
+// All start with describe
+describe('Instachain', function (){ // function as we are wrapping the test suite into a function
+    // Test cases for the Instachain contract
+    it('should deploy the Instachain contract', async function(){
+        // Get a blueprint for the contract
+        const Instachain = await ethers.getContractFactory('InstaChain');
+        // Deploy the contract using the blueprint
+        const instachain = await Instachain.deploy();
+        // Wait for the deployment to finish
+        await instachain.deployed();
+        // Check if the contract address is not zero if zero it means the contract was not deployed
+        expect(instachain.address).to.not.equal(0);
+    })
+
+    it('should create a new post', async function() {
+        // call the instachain contract
+        const Instachain = await ethers.getContractFactory('InstaChain');
+        // Deploy the contract
+        const instachain = await Instachain.deploy();
+        // Check deployment has happened
+        await instachain.deployed();
+        expect(instachain.address).to.not.equal(0);
+        // create a new post
+        await instachain.createPost("This is a test post","London", "northeaster.psg");
+    })
+
+    it('Should have the correct post id', async function(){
+        // Call the instachain contract
+        const Instachain = await ethers.getContractFactory('InstaChain');
+        // Deploy the contract
+        const instachain = await Instachain.deploy();
+        // Check deployment has happened
+        await instachain.deployed();
+        expect(instachain.address).to.not.equal(0);
+
+        // create a new post
+
+        const caption = 'This is a test post, id should be 2';
+        const location = 'London';
+        const imageHash = 'northeastern.psg';
+
+        await instachain.createPost(caption, location, imageHash);
+
+        // Check if the post count is 2
+        const postCount = await instachain.postCount();
+        expect(postCount.toNumber()).to.equal(1);
+
+    })
+})
