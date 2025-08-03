30c9c9cc2580d889952ad56ae949d0f41cb4bfdd
Umar <umar@mac.home>
2025-08-03 21:27:44 +0100

    Move built files to root for GitHub Pages

--- instachain-frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/instachain/' : '/',
-})
